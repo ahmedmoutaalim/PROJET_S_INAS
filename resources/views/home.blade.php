89d486f1d54d2bd26468359e61178d472004939a
Omar <Omar@omar.com>
2020-07-16 19:47:57 +0100

    add menu

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,9 +1,25 @@
 @extends('layouts.app')
 
 @section('content')
-<div style="display: flex; width: 100%; justify-content: space-between; " >
-<img src="images/logo-inas.png" style="width: 300px; height:50px">
-<h1 style="color: white; background-color:red;padding:10px">Préinscription en ligne</h1>
+
+<div style="display: flex; width: 100%;height: 80px; justify-content: space-between ;font-family: 'Work Sans', sans-serif; border-bottom:1.1px solid rgb(245,245,245); " >
+<img src="images/logo-inas.png" style=" max-width: 225px;cursor:pointer; height:35px ; margin:26px;">
+<button style="color: white; background-color:#FF3838 ;width:19%; font-size:17px; ; cursor:pointer; border:none;">PRÉINSCRIPTON EN LIGNE </button>
 </div>
 
+@endsection
+
+@section('menu')
+<div></div>
+<ul> 
+<li> <a href="#">ACCUEIL</a> </li>
+<li> <a href="#">PRESENTATION DE L’INAS</a> </li>
+<li> <a href="#">FORMATION</a> </li>
+<li> <a href="#">ACTUALITES</a></li>
+<li> <a href="#">ETUDES ET RECHERCHE</a> </li>
+<li> <a href="#">COOPERATION ET PARTENARIAT</a> </li>
+<li> <a href="#"> DOCUMENTATION</a></li>
+<li> <a href="#">ESPACE ENSEIGNANT</a> </li>
+</ul>
+</div>
 @endsection
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,17 +1,38 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
+   
     <meta charset="UTF-8">
+    <link href="https://fonts.googleapis.com/css2?family=Work+Sans&display=swap" rel="stylesheet">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   
     <title>INAS</title>
     <style>
         *{
             padding: 0;
             margin: 0;
+            list-style:none;
+            text-decoration:none;
+            font-family: 'Work Sans', sans-serif;
+        }
+        
+        ul{
+            margin-top: 30px;  
+        }
+
+        li{
+            display: initial;
+            padding:18px;
+        }
+        a{
+            color:#757575;
         }
     </style>
+
 </head>
 <body>
     @yield('content')
+    @yield('menu')
+    
 </body>
 </html>
