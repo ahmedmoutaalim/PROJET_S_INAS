556638aaa137173d4cb1c4d67e5456d9b266be7e
mdzoubir <57962405+mdzoubir@users.noreply.github.com>
2020-07-16 01:12:34 +0100

    start code home page

---
+++ resources/views/home.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.app')
+
+@section('content')
+<div style="display: flex; width: 100%; justify-content: space-between; " >
+<img src="images/logo-inas.png" style="width: 300px; height:50px">
+<h1 style="color: white; background-color:red;padding:10px">Préinscription en ligne</h1>
+</div>
+
+@endsection
