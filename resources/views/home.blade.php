2a9a75841db9b8131304e13fa413713a067aaf63
mdzoubir <57962405+mdzoubir@users.noreply.github.com>
2020-07-18 01:28:04 +0100

    ajouter slider

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,5 +2,19 @@
 
 
 @section('accueil')
-
+<div class="sliderA">
+    <div class="slider-items">
+        <div class="item active">
+             <img src="//inastanger.ma/wp-content/uploads/2020/07/unnamed.jpg">
+             
+        </div>
+        <div class="item">
+             <img src="img/slide2.jpg">
+        </div>
+    </div>
+    <!-- slider controls -->
+      <div class="left-slide">&lt;</div>
+      <div class="right-slide">&gt;</div>
+    <!-- slider controls -->
+</div>
 @endsection
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <link href="https://fonts.googleapis.com/css2?family=Work+Sans&display=swap" rel="stylesheet">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-   
+    <!-- <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous"> -->
     <title>INAS</title>
     <style>
         *{
@@ -17,70 +17,112 @@
         }
         
 
-        .fachkar{
+        .container{
             margin-top:10px;
         }
-.samkar{
+        .parent-list{
 
         display: inline-block;
         padding: 20px;
-      }
+        }
 
-      .samkar a{
-         
-        
-          font-size:15.6px;
-          color:#757575;
-      }
+        li a{
+          
+          
+            font-size:15.6px;
+            color:#757575;
+        }
 
-      .korbar::after{
-        content:'';
-          height:2.3px;
-          background:#FF3838;
-          display:block;
-          width:30%;
-          margin-top:4px;
-      }
-      .korbar:hover{
-        color:#FF3838;
-        transition:  .5s;
-      }
-
-      .aanbar::after{
+        .normal::after{
           content:'';
-          display:block;
-          width:0%;
-          height:2.3px;
-          background:#FF3838;
-          margin-top:4px;
-          transition: width .5s;
-      }
-      .aanbar:hover::after {
-          width:20%;
-          transition: width .5s;
-         
-      }
-      .aanbar:hover{
-        transition:  .5s;
-        color:#FF3838;
-      }
+            height:2.3px;
+            background:#FF3838;
+            display:block;
+            width:30%;
+            margin-top:4px;
+        }
+        .normal:hover{
+          color:#FF3838;
+          transition:  .5s;
+        }
 
-      .zanbar{
+        .cool-link::after{
+            content:'';
+            display:block;
+            width:0%;
+            height:2.3px;
+            background:#FF3838;
+            margin-top:4px;
+            transition: width .5s;
+        }
+        .cool-link:hover::after {
+            width:20%;
+            transition: width .5s;
+          
+        }
+        .cool-link:hover{
+          transition:  .5s;
+          color:#FF3838;
+        }
 
-        float:left;
-        background-color:#333333;
-        color:#757575;
-        margin-top:20px;
-        display:none;
-       
-      }
-      .zanbar:hover{
-        display:block;
-        margi
-      }
+        .list-child{
+
+          float:left;
+          background-color:#333333;
+          color:#757575;
+        
+        }
 
      
-      
+      /* slider */
+        .sliderA{
+        width: 100%;
+        /* border:10px solid  #29a8e2; */
+        /* margin:80px auto; */
+        position: relative;
+        overflow: hidden;
+        }
+
+        .sliderA .left-slide,.sliderA .right-slide{
+          position: absolute;
+          height: 40px;
+          width: 40px;
+          background-color: #444444;
+          border-radius: 50%;
+          color:#ffffff;
+          font-size: 20px;
+          top:50%;
+          cursor: pointer;
+          margin-top: -20px;
+          text-align: center;
+          line-height: 40px;
+          
+        }
+        .sliderA .left-slide:hover,.sliderA .right-slide:hover{
+        box-shadow: 0px 0px 10px black;
+        background-color: #29a8e2;
+        }
+
+        .sliderA .left-slide{
+          left: 30px;
+        }
+        .sliderA .right-slide{
+          right: 30px;
+        }
+        .sliderA .slider-items .item img{
+          background:content-box;
+          /* height: 100%; */
+          display: block;
+        }
+
+        .sliderA .slider-items .item{
+          display: none;
+          position: relative;
+        }
+
+        .sliderA .slider-items .item.active{
+          display: block;
+        }
 
 
     </style>
@@ -92,36 +134,34 @@
 <button style="color: white; background-color:#FF3838 ;width:19%; font-size:17px; cursor:pointer; border:none; height:80px; margin-top:20px;">PRÉINSCRIPTON EN LIGNE </button>
 </div>
 
-<div class="fachkar">
+<div class="container">
 
-<li class="samkar"> <a href="#" class="korbar">ACCUEIL</a> </li>
-<li class="samkar"> <a href="#" class="aanbar">PRESENTATION DE L’INAS</a>
-    <div class="zanbar">
+<li class="parent-list"> <a href="#" class="normal">ACCUEIL</a> </li>
+<li class="parent-list"> <a href="#" class="cool-link">PRESENTATION DE L’INAS</a>
+    <div class="list-child">
      <ul>
-     <li>PRESENTATION DE L’INAS</li>
-     <li>MOT DE DIRECTRICE</li>
-     <li>MISSION</li>
-     <li>HISTORIQUE</li>
-     <li>Instance de gouvernance </li>
-     <li>Filières</li>
-     <li>Deprtements</li>
-     <li>INAS EN CHIFFRES</li>
+     <li>llkkk</li>
+     <li>socsokc</li>
+     <li>skcksc</li>
      </ul>
     
     </div>
  </li>
-<li class="samkar"> <a href="#" class="aanbar">FORMATION</a> </li>
-<li class="samkar"> <a href="#" class="aanbar">ACTUALITES</a></li>
-<li class="samkar"> <a href="#" class="aanbar">ETUDES ET RECHERCHE</a> </li>
-<li class="samkar"> <a href="#" class="aanbar">COOPERATION ET PARTENARIAT</a> </li>
-<li class="samkar"> <a href="#" class="aanbar"> DOCUMENTATION</a></li>
-<li class="samkar"> <a href="#" class="aanbar">ESPACE ENSEIGNANT</a> </li>
+<li class="parent-list"> <a href="#" class="cool-link">FORMATION</a> </li>
+<li class="parent-list"> <a href="#" class="cool-link">ACTUALITES</a></li>
+<li class="parent-list"> <a href="#" class="cool-link">ETUDES ET RECHERCHE</a> </li>
+<li class="parent-list"> <a href="#" class="cool-link">COOPERATION ET PARTENARIAT</a> </li>
+<li class="parent-list"> <a href="#" class="cool-link"> DOCUMENTATION</a></li>
+<li class="parent-list"> <a href="#" class="cool-link">ESPACE ENSEIGNANT</a> </li>
 
 </div>
     @yield('accueil')
 
 
     <script src="../../resources/js/main.js"></script>
-    
+    <!-- <script src="{{ asset('js/slider.js')}}"></script> -->
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
 </body>
 </html>
