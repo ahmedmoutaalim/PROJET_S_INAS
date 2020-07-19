5c1cec7776a4c33b88ba20e795fe103453c7a1fd
Omar <Omar@omar.com>
2020-07-19 16:21:03 +0100

    add section liens

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -28,10 +28,10 @@
 
 @section('présontation')
 
-<div style="display:flex; justify-content: space-around;  ">
+<div class="flex">
 
      <div class='flex_firstText'>
-         <h1 style=" color:#333333;font-weight:large;">PRÉSENTATION DE L’INAS</h1>
+         <h1 class="flex_firstText-title">PRÉSENTATION DE L’INAS</h1> <br>
            <p class='flex_firstText-content'>L’Institut National de l’Action Sociale est un établissement 
                                            d’enseignement supérieur ne relevant pas des universités,
                                            crée en 1981 par l’autorité gouvernementale chargée des
@@ -40,21 +40,42 @@
                                            social de grande renommée nationale et africaine. Il forme des cadres supérieurs et 
                                            moyens spécialisés en travail social et mène actuellement un  grand chantier de restructuration
                                            et développement pour diversifier son offre de formation et augmenter ses effectifs
-                                           d’étudiants et intégrer la formation à l’international.
+                                           d’étudiants et intégrer la formation à l’international. <br>
                                            L’institut relève actuellement du Ministère de la Solidarité
                                            du Développement Social, de l’Egalité et de la Famille.</p>
      </div>
 
      <div class='flex_secondText'>
-          <h1 class='flex_secondText-title'>Mot de la Directrice de l’INAS</h1>
+          <h1 class='flex_secondText-title'>Mot de la Directrice de l’INAS</h1> <br>
             <P class='flex_secondText-content'>
-                                           Actuellement, le domaine du travail social est en plein essor, et en perpétuelle évolution, ce qui en fait un choix de formation convoité par les professionnels en devenir.
+                                           Actuellement, le domaine du travail social est en plein essor, et en perpétuelle évolution, ce qui en fait un choix de formation convoité par les professionnels en devenir. <br> 
 
-                                           Dans ce sens, l’Institut National de l’Action Sociale contribue à la formation de personnes compétentes et réflexives, dotées d’un sens critique et éthique aiguisé qui deviendront des acteurs de changement pour le mieux-être des personnes.
+                                           Dans ce sens, l’Institut National de l’Action Sociale contribue à la formation de personnes  compétentes et réflexives, dotées d’un sens critique et éthique aiguisé qui deviendront des acteurs de changement pour le mieux-être des personnes. <br> 
 
-                                           Milieu d’échange stimulant, notre institut adhère à des valeurs et à des principes de justice sociale et favorise la promotion du développement social et du respect de la diversité humaine, dans un esprit d’ouverture et de dialogue. Il favorise la collaboration interprofessionnelle centrée sur les personnes, les familles, les groupes et les collectivités auprès desquels les professionnels interviennent.
+                                           Milieu d’échange stimulant, notre institut adhère à des valeurs et à des principes de justice sociale et favorise la promotion du développement social et du respect de la diversité humaine, dans un esprit d’ouverture et de dialogue. Il favorise la collaboration interprofessionnelle centrée sur les personnes, les familles, les groupes et les collectivités auprès desquels les professionnels interviennent. <br> 
 
                                            L’INAS s’appuie également sur une diversité de perspectives théoriques et méthodologiques, ainsi que sur les savoirs issus des pratiques professionnelles.</P>
+    </div>
+
+    <div class="flex_imgs">
+    <img  class="flex_imgs-self" width="222" height="100" src="http://inastanger.ma/wp-content/uploads/2020/07/srceen100720.png"  alt="img1">
+    <img  class="flex_imgs-self" width="223" height="100" src="http://inastanger.ma/wp-content/uploads/2020/07/unnamed-03072020.png"  alt="img2">
+    <img  class="flex_imgs-self" width="222" height="100" src="http://inastanger.ma/wp-content/uploads/2020/07/srceen100720.png"  alt="img1">
+    <img  class="flex_imgs-self" width="223" height="100" src="http://inastanger.ma/wp-content/uploads/2020/07/unnamed-03072020.png"  alt="img2">
+    </div>
+
+    <div class="flex_liens">
+
+    <h1> LIENS UTILES</h1>
+    <img  width="150" height="80" src="http://inastanger.ma/wp-content/uploads/2020/07/e-chiqayasite.png" alt="">
+    <img   width="150" height="80"   src="http://inastanger.ma/wp-content/uploads/2020/07/visuel-4_1.jpg" alt="">
+    <img  width="150" height="80"   src="http://inastanger.ma/wp-content/uploads/2020/07/visuel-4_1.jpg" alt="">
+    <img  width="150" height="80"    src="http://inastanger.ma/wp-content/uploads/2020/07/visuel-4_1.jpg" alt="">
+    <img  width="150" height="80" src="http://inastanger.ma/wp-content/uploads/2020/07/img1arcampagne.jpg" alt="">
+    <img  width="150" height="80" src="http://inastanger.ma/wp-content/uploads/2020/07/img1arcampagne.jpg" alt="">
+    
+
+
     </div>
 
 </div>
