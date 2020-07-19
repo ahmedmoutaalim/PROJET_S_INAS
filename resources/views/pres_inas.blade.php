67249a01ea181adb4dd326cde0c4ca35e10103ff
mdzoubir <57962405+mdzoubir@users.noreply.github.com>
2020-07-19 11:12:57 +0100

    Update pres_inas.blade.php

--- resources/views/pres_inas.blade.php
+++ resources/views/pres_inas.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.app')
 
 @section('accueil')
-<div  style="background-image: url(http://inastanger.ma/wp-content/uploads/2020/07/fdcgfd.jpg)" >
+<div  style="background-color:red;      background-image: url(http://inastanger.ma/wp-content/uploads/2020/07/fdcgfd.jpg)" >
     <div class="row p-5" style="color: whitesmoke;">
         <h1 class="col">Présentation générale de L'INAS</h1>
         <div class="col pt-3" style="text-align: center;"><p><a href="#" style="color: #d7ccc8;"> Accueil ></a> Présentation générale de L'INAS</p></div>
