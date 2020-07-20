9c97fc5d0cfa84934242be98e7e43b86ae00f3b4
mdzoubir <57962405+mdzoubir@users.noreply.github.com>
2020-07-20 12:58:48 +0100

    add w-pages

--- resources/views/pres_inas.blade.php
+++ resources/views/pres_inas.blade.php
@@ -2,7 +2,7 @@
 
 @section('accueil')
 <div  style="background-color:red;      background-image: url(http://inastanger.ma/wp-content/uploads/2020/07/fdcgfd.jpg)" >
-    <div class="row p-5" style="color: whitesmoke;">
+    <div class="row p-5 w-100" style="color: whitesmoke;">
         <h1 class="col font-weight-bold">Présentation générale de L'INAS</h1>
         <div class="col pt-3 text-center font-weight-bold "><p><a href="/" style="color: #d7ccc8; "> Accueil ></a> Présentation générale de L'INAS</p></div>
     </div>
