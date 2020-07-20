0a86961b3a52373590929d9c6372444da24d1520
Omar <Omar@omar.com>
2020-07-20 01:30:29 +0100

    add footer

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,7 +7,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
     <title>INAS</title>
-    <link rel="stylesheet" type="text/css" href="{{ asset('css/globle.css')}}">
+    <link rel="stylesheet" type="text/css" href="{{ asset('css/style.css')}}">
 </head>
 <body>
 <div style="display: flex; width: 100%;height: 120px; justify-content: space-between ;font-family: 'Work Sans', sans-serif; border-bottom:1.1px solid rgb(245,245,245); " >
@@ -17,7 +17,7 @@
 
 <div class="menu">
 <ul>
-<li class="menu_list"> <a href="/" class="menu_list-accueil">ACCUEIL</a> </li>
+<li class="menu_list"> <a href="#" class="menu_list-accueil">ACCUEIL</a> </li>
 <li class="menu_list"> <a href="#" class="menu_list-parentlist">PRESENTATION DE L’INAS</a>
     <div class="menu_list-childlist">
      <ul>
@@ -113,22 +113,42 @@
 </li>
 </ul>
 </div>
-
-
     @yield('accueil')
     @yield('présontation')
 
 
- <div>
 
- 
+    
+ <div class="footer">
+    <div class="footer_links">
+       <a class="footer_links-hover"  href="#">PRESENTATION DE L’INAS</a>
+       <a class="footer_links-hover" href="#">FORMATION</a>
+       <a class="footer_links-hover" href="#">ETUDES ET RECHERCHE</a>
+       <a class="footer_links-hover" href="#">DOCUMENTATION</a>
+
+    </div>  
+    <div  class="footer_linkssecond" >
+    <a class="footer_links-hover" href="#">ACTUALITES</a>
+       <a class="footer_links-hover" href="#">COOPERATION ET PARTENARIAT</a>
+       <a class="footer_links-hover" href="#">Liens Utiles</a>
+
+    </div>   
+    
+    <div class="footer_fond">
+    <hr>
+
+      <p class="endFooter">© 2020 Institut National de l'Action Sociale 
+        (INAS) | Ministère de la solidarité de la femme 
+        de la famille et du développement social au Maroc</p>
+
+    </div>
  </div>   
 
 
-    <script src="../../resources/js/main.js"></script>
+    <!-- <script src="../../resources/js/main.js"></script>
     <!-- <script src="{{ asset('js/slider.js')}}"></script> -->
-    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <!-- <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>  -->
 </body>
 </html>
