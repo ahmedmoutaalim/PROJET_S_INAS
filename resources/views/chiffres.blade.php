0958898e01fc24ce79e606ae1da5d0756e44963b
mdzoubir <57962405+mdzoubir@users.noreply.github.com>
2020-07-21 15:32:15 +0100

    realisation de la page inas en chiffres

---
+++ resources/views/chiffres.blade.php
@@ -0,0 +1,128 @@
+@extends('layouts.app')
+
+@section('accueil')
+<div  style="background-color:red;      background-image: url(http://inastanger.ma/wp-content/uploads/2020/07/fdcgfd.jpg)" >
+    <div class="row p-5 w-100" style="color: whitesmoke;">
+        <h1 class="col font-weight-bold">INAS en chiffers</h1>
+        <div class="col pt-3 text-center font-weight-bold"><p><a href="/" style="color: #d7ccc8;"> Accueil ></a> INAS en chiffers</p></div>
+    </div>
+</div>
+<div class="m-5 " style="color: black; width:80%; line-height: 30px; letter-spacing: .01em;">
+    <div class="mt-5 ">   
+        <p class="font-weight-bold">Année universitaire 2019-2020</p>
+        <p class="font-weight-bold">Filière « licence professionnelle en Travail social »</p>
+    
+    
+        <table class="table">
+            <thead>
+                <tr>
+                    <th scope="col" class="font-weight-bold">Niveau d’études</th>
+                    <th scope="col" class="font-weight-bold">Nombre d’étudiants</th>
+                </tr>
+            </thead>
+            <tbody  style="font-weight: 300;">
+                <tr>
+                    <th scope="row">Première année</th>
+                    <td>59</td>
+                </tr>
+                <tr>
+                    <th scope="row">Quatrième année (ancien système)</th>
+                    <td>34</td>
+                </tr>
+            </tbody>
+        </table>
+        <hr>
+    </div>
+    <div class="mt-5 ">
+        <div class="font-weight-bold">
+            <p>Diplômés de l’INAS</p>
+            <p>Depuis sa création</p>
+            <p>1899 diplômés</p>
+        </div>
+        <table class="table">
+            <thead>
+                <tr>
+                    <th scope="col" class="font-weight-bold">Gestionnaires en travail social<br>
+                    2ème cycle</th>
+                    <th scope="col" class="font-weight-bold">Travailleurs sociaux <br>
+                    1er cycle</th>
+                </tr>
+            </thead>
+            <tbody  style="font-weight: 300;">
+                <tr>
+                    <th scope="row">1597</th>
+                    <td>302</td>
+                </tr>
+                
+            </tbody>
+        </table>
+        <hr>
+    </div>
+    <div class="mt-5 ">
+        <div class="font-weight-bold">
+            <p>Etudiants étrangers</p>
+            <p>Depuis sa création</p>
+            <hr>
+            <p>76 étudiants étrangers de 19 nationalités Africaines</p>
+            <hr>
+        </div>
+        
+    </div>
+    <div class="mt-5 ">
+        <p class="font-weight-bold">Autres bénéficiaires de la formation  à l’INAS</p>
+        <table class="table">
+            <thead>
+                <tr>
+                    <th scope="col" class="font-weight-bold">Secteur</th>
+                    <th scope="col" class="font-weight-bold">Nombre</th>
+                </tr>
+            </thead>
+            <tbody  style="font-weight: 300;">
+                <tr>
+                    <th scope="row">Les Forces armées royales</th>
+                    <td>104 assistantes sociales</td>
+                </tr>
+                <tr>
+                    <th scope="row">La Sureté nationales</th>
+                    <td>11 assistantes sociales</td>
+                </tr>
+                <tr>
+                    <th scope="row">Ministère de la défense nationale</th>
+                    <td>17 sous officiers</td>
+                </tr>
+            </tbody>
+            <hr>
+        </table>
+    </div>
+    <div class="mt-5 font-weight-bold">
+        <p style="margin: 20px 0;">Infrastructure de l’INAS</p>
+        <hr>
+        <p style="margin: 20px 0;">5 classes de cours</p>
+        <hr>
+        <p style="margin: 20px 0;">Une grande bibliothèque spécialisée</p>
+        <hr>
+        <p style="margin: 20px 0;">Une salle informatique</p>
+        <hr>
+        <p style="margin: 20px 0;">Une salle de lecture</p>
+        <hr>
+        <p style="margin: 20px 0;">Une grande salle de conférences et de grandes activités</p>
+        <hr>
+        <p style="margin: 20px 0;">Une salle d’infirmerie</p>
+        <hr>
+        <p style="margin: 20px 0;">5 dortoirs</p>
+        <hr>
+        <p style="margin: 20px 0;">Un réfectoire</p>
+        <hr>
+        <p style="margin: 20px 0;">Une cantine</p>
+        <hr>
+        <p style="margin: 20px 0;">Une mosquée</p>
+        <hr>
+        <p style="margin: 20px 0;">Une salle de professeurs</p>
+        <hr>
+        <p style="margin: 20px 0;">Un grand espace d’activités sportives</p>
+        <hr>
+        <p style="margin: 20px 0;">11 bureaux administratifs</p>
+        <hr>
+    </div>
+</div>
+@endsection
